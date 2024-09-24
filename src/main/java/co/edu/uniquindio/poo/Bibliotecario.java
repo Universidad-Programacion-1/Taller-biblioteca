43a148845bf056103915fe3802ea3c6084b8654f
Carlos Ruiz <carlosa.ruiza@uqvirtual.edu.co>
2024-09-24 18:47:27 -0500

    Creacion de los atributos de la clase Bibliotecario

--- src/main/java/co/edu/uniquindio/poo/Bibliotecario.java
+++ src/main/java/co/edu/uniquindio/poo/Bibliotecario.java
@@ -1,5 +1,35 @@
 package co.edu.uniquindio.poo;
 
-public class Bibliotecario {
+import java.util.Date;
+
+public class Bibliotecario extends Persona{
     
+    private int salarioBibliotecario;
+    private Date fechaContratacion;
+
+    public Bibliotecario( Persona persona, int salarioBibliotecario, Date fechaContratacion) {
+        this.salarioBibliotecario = salarioBibliotecario;
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public int getSalarioBibliotecario() {
+        return salarioBibliotecario;
+    }
+
+    public void setSalarioBibliotecario(int salarioBibliotecario) {
+        this.salarioBibliotecario = salarioBibliotecario;
+    }
+
+    public Date getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(Date fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    
+
+    
+
 }
